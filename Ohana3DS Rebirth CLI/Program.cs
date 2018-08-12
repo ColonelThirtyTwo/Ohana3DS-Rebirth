a6939e323a22623f49060627562ee3bd15ef6dae
Alex Parrill <initrd.gz@gmail.com>
2018-08-11 22:17:00 -0400

    Add VerbBase superclass for CLI options
    Cleans up main() by moving the subcommand options to the class that implements
    the options.

--- Ohana3DS Rebirth CLI/Program.cs
+++ Ohana3DS Rebirth CLI/Program.cs
@@ -6,7 +6,22 @@ using System.IO;
 
 namespace Ohana3DS_Rebirth_CLI
 {
-    class InfoOptions
+    /// <summary>
+    /// Base class for this program's subcommands.
+    /// </summary>
+    abstract class VerbBase
+    {
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        /// <returns>Result code</returns>
+        public abstract int Execute();
+    }
+
+    /// <summary>
+    /// `info` command, displays file info
+    /// </summary>
+    class InfoOptions : VerbBase
     {
         [ValueOption(0)]
         public string InFile { get; set; }
@@ -18,15 +33,70 @@ namespace Ohana3DS_Rebirth_CLI
             help.AddPreOptionsLine("\nUsage: info <in-file>");
             return help;
         }
-    }
 
-    class ExportOptions
-    {
-        [ValueOption(0)]
-        public string InFile { get; set; }
+        public override int Execute()
+        {
+            if (this.InFile == null) // Dear CommandLineParser v1.9, please support required positional arguments
+            {
+                Console.WriteLine("Missing input file");
+                return Parser.DefaultExitCodeFail;
+            }
 
-        [ValueOption(1)]
-        public string OutDirectory { get; set; }
+            FileIO.file file;
+            try
+            {
+                file = FileIO.load(this.InFile);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Could not open file: " + ex.Message);
+                return 1;
+            }
+            switch (file.type)
+            {
+                case FileIO.formatType.model:
+                    var model_group = (RenderBase.OModelGroup)file.data;
+                    Console.WriteLine("File type: model");
+
+                    if (model_group.model.Count != 0)
+                    {
+                        Console.WriteLine("Models:");
+                        uint model_i = 1;
+                        foreach (var model in model_group.model)
+                        {
+                            Console.WriteLine(String.Format("{0}: {1} ({2} meshes, {3} vertices)", model_i, model.name, model.mesh.Count, model.verticesCount));
+                            model_i++;
+                        }
+                    }
+
+                    if (model_group.texture.Count != 0)
+                    {
+                        Console.WriteLine("Textures:");
+                        uint i = 1;
+                        foreach (var texture in model_group.texture)
+                        {
+                            Console.WriteLine(String.Format("{0}: {1}", i, texture.name));
+                            i++;
+                        }
+                    }
+
+                    if (model_group.skeletalAnimation.Count != 0)
+                    {
+                        Console.WriteLine("Skeletal Animations:");
+                        uint anim_i = 1;
+                        foreach (var animation in model_group.skeletalAnimation)
+                        {
+                            Console.WriteLine(String.Format("{0}: {1} ({2} frames)", anim_i, animation.name, animation.frameSize));
+                            anim_i++;
+                        }
+                    }
+
+                    return 0;
+                default:
+                    Console.WriteLine("Unrecognized file");
+                    return 1;
+            }
+        }
     }
 
     class Options
@@ -65,78 +135,9 @@ namespace Ohana3DS_Rebirth_CLI
             object sub_options = null;
             if(!Parser.Default.ParseArgumentsStrict(args, options, (the_verb, the_options) => { verb = the_verb; sub_options = the_options; }))
             {
-                Environment.Exit(Parser.DefaultExitCodeFail);
+                return Parser.DefaultExitCodeFail;
             }
-            
-            if(verb == "info")
-            {
-                var info_options = (InfoOptions)sub_options;
-                if (info_options.InFile == null) // Dear CommandLineParser v1.9, please support required positional arguments
-                {
-                    Console.WriteLine("Missing input file");
-                    return Parser.DefaultExitCodeFail;
-                }
-
-                FileIO.file file;
-                try
-                {
-                    file = FileIO.load(info_options.InFile);
-                }
-                catch (IOException ex)
-                {
-                    Console.WriteLine("Could not open file: " + ex.Message);
-                    return 1;
-                }
-                switch (file.type)
-                {
-                    case FileIO.formatType.model:
-                        var model_group = (RenderBase.OModelGroup)file.data;
-                        Console.WriteLine("File type: model");
-
-                        if (model_group.model.Count != 0)
-                        {
-                            Console.WriteLine("Models:");
-                            uint model_i = 1;
-                            foreach (var model in model_group.model)
-                            {
-                                Console.WriteLine(String.Format("{0}: {1} ({2} meshes, {3} vertices)", model_i, model.name, model.mesh.Count, model.verticesCount));
-                                model_i++;
-                            }
-                        }
-
-                        if (model_group.texture.Count != 0)
-                        {
-                            Console.WriteLine("Textures:");
-                            uint i = 1;
-                            foreach (var texture in model_group.texture)
-                            {
-                                Console.WriteLine(String.Format("{0}: {1}", i, texture.name));
-                                i++;
-                            }
-                        }
-
-                        if (model_group.skeletalAnimation.Count != 0)
-                        {
-                            Console.WriteLine("Skeletal Animations:");
-                            uint anim_i = 1;
-                            foreach (var animation in model_group.skeletalAnimation)
-                            {
-                                Console.WriteLine(String.Format("{0}: {1} ({2} frames)", anim_i, animation.name, animation.frameSize));
-                                anim_i++;
-                            }
-                        }
-
-                        return 0;
-                    default:
-                        Console.WriteLine("Unrecognized file");
-                        return 1;
-                }
-            }
-            else
-            {
-                throw new Exception("Unrecognized verb: " + verb);
-            }
-            
+            return ((VerbBase)sub_options).Execute();
         }
     }
 }
